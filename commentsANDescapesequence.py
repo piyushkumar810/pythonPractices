cb4f2f5e19ec2317de3913b5053da3b0340233b1
piyushkumar810 <140537940+piyushkumar810@users.noreply.github.com>
2024-07-25 09:52:45 +0530

    added new program

---
+++ commentsANDescapesequence.py
@@ -0,0 +1,29 @@
+print ("lerning comments in python")
+print("single line comment") 
+# this is comment line which is understand by the programmers not by the computer
+
+print("single line comment") #is this will work 
+
+print("multiline line comment")
+
+''' hi piyush 
+you are using multi line comment in your code
+this is for programmers
+'''
+
+""" hi piyush 
+you are using multi line comment in your code
+this is for programmers
+"""
+
+
+print("Ecsape sequence character")
+print("when you want to include new line in your output in single print statement \n then use escape sequence character in your print statement")
+
+
+print("Double quote scape sequence character")
+print("hellow piyush \n you are learning\"python\" \n it is simple programming language ")
+
+print("hello", 6, 2*7)
+print("we can also use seperator to seprate it")
+print("hello", 6, 2*7, sep="~")
