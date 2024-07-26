ea8497f8a6a7b1d17dda5d18bd6d88194f2c7e9e
piyushkumar810 <140537940+piyushkumar810@users.noreply.github.com>
2024-07-26 07:08:52 +0530

    simple calculator

---
+++ simpleCalculator.py
@@ -0,0 +1,8 @@
+a=10
+b=20
+
+print("the value of ",a,"+",b, "=", a+b)
+print("the value of ",a,"-",b, "=", a-b)
+print("the value of ",a,"*",b, "=", a*b)
+print("the value of ",a,"/",b, "=", a/b)
+print("the value of ",a,"%",b, "=", a%b)
