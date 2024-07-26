e42e509dcd05e38cc231e8ef88551a33d0c45e61
piyushkumar810 <140537940+piyushkumar810@users.noreply.github.com>
2024-07-26 07:03:56 +0530

    variable and data type

---
+++ vaiablesANDdatatype.py
@@ -0,0 +1,28 @@
+a=5
+b="piyush"
+
+print("the value of a is = ", a)
+print("the value of b is = ", b)
+
+# some built in data types are :-
+
+print("Numeric data :- int, float, complex")
+print("Text data :- str")
+print("Boolean data :- true or flase")
+print("Sequenced data :- list, tuple")
+print("Mapped data :- dict")
+
+#examples
+a1=5
+a2=7.457
+a3="piyush"
+a4=True
+a5=[5,"piyush",[-4,0,"hello"]]
+a6={"name":"piyush", "age":22, "canVote":True}
+
+print("the type of a1 is ", type(a1))
+print("the type of a2 is ", type(a2))
+print("the type of a3 is ", type(a3))
+print("the type of a4 is ", type(a4))
+print("the type of a5 is ", type(a5))
+print("the type of a6 is ", type(a6))
