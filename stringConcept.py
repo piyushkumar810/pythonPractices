384772a8bf547f44706c2d1b3f6d3eebdc436192
piyushkumar810 <140537940+piyushkumar810@users.noreply.github.com>
2024-07-28 08:27:13 +0530

    stringConcept

---
+++ stringConcept.py
@@ -0,0 +1,25 @@
+# ---------------------------------------------concept of string
+
+print('anything that enclosed between single and double quote is considered as string')
+# eg 1
+name="piyush kr"
+print('hello', name)
+
+# name1="he said, "i want to eat apple""
+# here pyphton will confuse which double quote is used were to solve this you can use either scape sequence or single quote
+name2='he said, "i want to eat apple"'
+name3="he said, \"i want to eat apple\""
+print(name2)
+print(name3)
+
+# ---------------------------------------how to use multiline string
+print("we can use multiline string by enclosing that string inside triple single or double quote")
+# eg 2
+cow=""" cow is a animal
+    he has four legs , two ears and one tail
+    he gives milk which contain neutrants in it"""
+print(cow)
+
+
+
+
