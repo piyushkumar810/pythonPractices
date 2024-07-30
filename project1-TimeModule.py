98c2bafc40c955ab848edd2e9888659f84b8c6ee
piyushkumar810 <140537940+piyushkumar810@users.noreply.github.com>
2024-07-30 20:22:32 +0530

    project1

---
+++ project1-TimeModule.py
@@ -0,0 +1,5 @@
+# fetching time module and according to that printing good morning, good afternoon, good evening and good night  
+
+import time
+timestamp=time.strftime("%H:%M:%S")
+print(timestamp)
