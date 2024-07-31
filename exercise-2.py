c5b01923effcdf82ac6738b82a7e95862fdd32fd
piyushkumar810 <140537940+piyushkumar810@users.noreply.github.com>
2024-07-31 08:18:50 +0530

    made new one

---
+++ exercise-2.py
@@ -0,0 +1,28 @@
+# ----------------------------------------foreign time zone
+
+from datetime import datetime
+import pytz
+
+# Define the foreign time zone (e.g., 'America/New_York')
+foreign_timezone = pytz.timezone('America/New_York')
+
+# Get the current time in the foreign time zone
+foreign_time = datetime.now(foreign_timezone)
+
+# Format the time to include AM/PM
+formatted_time = foreign_time.strftime("%I:%M:%S %p")
+
+# Determine the appropriate greeting based on the hour
+hour = foreign_time.hour
+
+if 5 <= hour < 12:
+    greeting = "Good Morning"
+elif 12 <= hour < 17:
+    greeting = "Good Afternoon"
+elif 17 <= hour < 21:
+    greeting = "Good Evening"
+else:
+    greeting = "Good Night"
+
+# Print the greeting and current time
+print(f"{greeting}, the current time is {formatted_time} in the foreign time zone.")
