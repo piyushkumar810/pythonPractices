94c91b5ea65d6a52473b3a7ca6c774a5be79d30a
piyushkumar810 <140537940+piyushkumar810@users.noreply.github.com>
2024-08-01 21:13:33 +0530

    while loop in python

---
+++ whileLoop.py
@@ -0,0 +1,7 @@
+# ------------------------------while loop in python
+
+i=int(input("Enter the number : "))
+while(i<=35):
+    i=int(input("Enter the number : "))
+    print(i)
+print("done with the loop")
