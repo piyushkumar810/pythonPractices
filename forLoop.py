c54a4faed81f22fbba079149ea71dad63523853b
piyushkumar810 <140537940+piyushkumar810@users.noreply.github.com>
2024-08-01 21:07:59 +0530

    for loop in python

---
+++ forLoop.py
@@ -0,0 +1,17 @@
+# ---------------------------for loop in python 
+
+# Q1) for loop in string
+name="piyush"
+for i in name:
+    print(i)
+
+# Q2) for loop in list
+color=["red", "yellow", "orange", "green", "black"]
+for j in color:
+    # print(j)
+    if(j=="green"):
+        print("yes you picked", j , "color")
+
+# Q3) range in for loop
+for k in range(100):
+    print(k)
