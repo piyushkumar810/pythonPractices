be8eacd7c200f46fa8c37617b87a1af2c8cfa626
piyushkumar810 <140537940+piyushkumar810@users.noreply.github.com>
2024-08-02 22:40:23 +0530

    break and continue statement

---
+++ BreakAndContinueStatement.py
@@ -0,0 +1,16 @@
+# ----------------------------------break and continue statement in python
+
+# Q1) for break statement : - break the loop
+for i in range(20):
+    print(i)
+    if(i==15):
+        print("now i is greater than ", i)
+        break
+        print("exit from loop")
+
+# Q2)for continue statement :- break the iteration
+for i in range(12):
+    if(i==10):
+        print("skip the iteration")
+        continue
+    print("5 * ",i, "=", 5*i)
