340231022afba91aaa4c02a927b737387bb504ac
piyushkumar810 <140537940+piyushkumar810@users.noreply.github.com>
2024-08-07 09:36:50 +0530

    list Practice

---
+++ listPractice.py
@@ -0,0 +1,18 @@
+question=[
+    ["what is your name ? ", "piyush","ragav","saurav","sonu", 1],
+    ["who developed c language ?", "denish ritchi", "benjreen strawstra","ata love lay", "piyush", 3],
+    ["what is your name ? ", "piyush","ragav","saurav","sonu", 1],
+    ["what is your name ? ", "piyush","ragav","saurav","sonu", 2],
+    ["what is your name ? ", "piyush","ragav","saurav","sonu", 4]
+        ]
+
+question1=question[0]
+print(question1)
+print("\n")
+
+question2=question[0][2]
+print(question2)
+print("\n")
+
+question3=question[0][5]
+print(question3)
