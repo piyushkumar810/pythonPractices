f4d921c93f096252dcf89ff0cd70b85209ee00f8
piyushkumar810 <140537940+piyushkumar810@users.noreply.github.com>
2024-08-10 17:03:34 +0530

    sets in python

---
+++ setInPython.py
@@ -0,0 +1,18 @@
+# ----------------------------------set in python
+
+# eg 1
+info={"piyush", 10, False, 30.6, 10, "india"}
+print(info) 
+print(type(info))
+
+# eg 2--> create an empty set
+piyush={}
+print(type(piyush))
+
+piyush1=set()
+print(type(piyush1))
+
+# eg 3 accessing set items
+info1={"piyush", 10, False, 30.6, 10, "india"}
+for i in info1:
+    print(i)
