81ed8d2eedb0971a4e253061e6df5598057c2436
piyushkumar810 <140537940+piyushkumar810@users.noreply.github.com>
2024-08-18 23:27:53 +0530

    encrypted Chatting

---
+++ Projects/project3-SecretCodeLanguage.py
@@ -0,0 +1,12 @@
+# write a program to translate a message into secret code language. use the rule below to translate normal english into secret cose language
+
+# coding
+# if the word contains at least 3 characters, remove the first letter and append it at the end 
+#          now append three randam character at ht estarting and the end 
+# else:
+#     simply reverse the string
+
+# decoding
+# if the word contain less than three characters then reverse it 
+# else: 
+#     remove three characters from start and end. now remove the last letter and append the last letter and append it to the beginning
