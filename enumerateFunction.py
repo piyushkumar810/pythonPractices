8ae9c9762672a80bca17f4ec3aa68a810ab3a8dd
piyushkumar810 <140537940+piyushkumar810@users.noreply.github.com>
2024-08-19 23:51:28 +0530

    enumerate function

---
+++ enumerateFunction.py
@@ -0,0 +1,24 @@
+# ------------------------------------enumerate function in python
+
+# problem faced before enumerate function
+# we face diffuculties in iterating (list, tuple ..) using there indexes because here we have to take a temprory variable to see index 
+# eg
+
+marks=[12, 56, 32, 98, 12, 45, 1, 4]
+index=0
+for mark in marks:
+    print(mark)
+    if(index == 3):
+        print("piyush, awesome ! ")
+    index +=1
+
+print("\n")
+# this temprory variable problem is solved by enumerate function
+# eg
+
+marks1=[12, 56, 32, 98, 12, 45, 1, 4]
+for i, mark1 in enumerate(marks1):
+    print(mark1)
+    if(i==3):
+        print("piyush, awesome ! ")
+
