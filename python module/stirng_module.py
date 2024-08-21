6879bba3485cf78fb65a382cde020c9c28f1368d
piyushkumar810 <140537940+piyushkumar810@users.noreply.github.com>
2024-08-21 15:02:42 +0530

    string module

---
+++ python module/stirng_module.py
@@ -0,0 +1,25 @@
+# -----------------------------------string module
+# python string module contains some constants, utility function and classes for string manipulation.
+
+import string as st
+
+# .ascii_letters
+print(st.ascii_letters)
+
+# .ascii_lowercase
+print(st.ascii_lowercase)
+
+# .ascii_uppercase
+print(st.ascii_uppercase)
+
+# .digits
+print(st.digits)
+
+# .hexdigit
+print(st.hexdigits)
+
+# .whitespaces
+print(st.whitespace)
+
+# .punctuation
+print(st.punctuation)
