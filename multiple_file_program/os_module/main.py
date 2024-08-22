9221531dad5ea8bd7fa0f7b143067fc959e7b0e3
piyushkumar810 <140537940+piyushkumar810@users.noreply.github.com>
2024-08-22 23:51:30 +0530

    updaated

--- multiple_file_program/os_module/main.py
+++ multiple_file_program/os_module/main.py
@@ -6,4 +6,8 @@ if(not os.path.exists("multiple_file_program/os_module/data")):
     os.mkdir("multiple_file_program/os_module/data")
 
 for i in range(0,10):
-    os.mkdir(f"multiple_file_program/os_module/data/Day{i+1}")
+    # os.mkdir(f"multiple_file_program/os_module/data/Day{i+1}")
+    # -------------------------renaming folders
+    # os.rename(f"multiple_file_program/os_module/data/Day{i+1}", f"multiple_file_program/os_module/data/Tutorial{i+1}")
+    # -------------------------suppose you want to give one space between tutorial and number
+    os.rename(f"multiple_file_program/os_module/data/Tutorial{i+1}", f"multiple_file_program/os_module/data/Tutorial {i+1}") 
