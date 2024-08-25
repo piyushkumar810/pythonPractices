0747213927222f3bf30e2f4763652c7c3481cadb
piyushkumar810 <140537940+piyushkumar810@users.noreply.github.com>
2024-08-25 20:41:37 +0530

    lambda funnction

---
+++ lambdaFunction.py
@@ -0,0 +1,24 @@
+# --------------------------------knowing lambda function
+# lambda function is an annomyous function (means a function without name) .it does not contain return statement
+
+# eg--> without using lambda function
+def sum(x,y):
+    return x+y
+a=sum(10,30)
+print(a)
+
+# eg --> using lambda function
+a1=lambda x1,y1:x1+y1
+print(a1)
+
+# eg3 -->passing function as an argument
+def cube(c):
+    return c*c*c
+print(cube(5))
+
+def appl(fx, value):
+    return 6+fx(value)
+
+print(appl(cube,2))
+# ------------or
+# print(appl(lambda x:x*x*x ,2))
