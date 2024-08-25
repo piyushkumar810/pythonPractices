1d41d0de9a9a72db8555ea02a40f2b9478b1cea8
piyushkumar810 <140537940+piyushkumar810@users.noreply.github.com>
2024-08-25 21:09:50 +0530

    filter function in python

--- FILTER_IN_PYTHON.py
+++ FILTER_IN_PYTHON.py
@@ -1,2 +1,16 @@
 # ----------------------------filter in pyhton
+# filter function filters a given sequence of element based on given condition and returns a new sequence
+
 # eg
+def filter_function(x):
+    return x>4
+
+list1=[4,6,8,2,9,5,3,0]
+newlist=list(filter(filter_function,list1))
+print(newlist)
+
+
+# using lambda function in filter function
+list2=[4,2,6,7,8,3,2,7]
+newlist1=list(filter(lambda x:(x>4), list2))
+print(newlist1)
