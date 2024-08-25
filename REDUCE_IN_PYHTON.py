b7616c8a991322c1b08cec89035fb69bbcad2efe
piyushkumar810 <140537940+piyushkumar810@users.noreply.github.com>
2024-08-25 21:15:18 +0530

    reduce in pyhton

---
+++ REDUCE_IN_PYHTON.py
@@ -0,0 +1,12 @@
+# ----------------------reduse in pyhton
+# the reduce function is applies a function to a sequence and retuerns a single value
+
+# eg
+from functools import reduce
+list1=[4,6,8,3,5,8,3]
+ 
+def sum(x,y):
+    return x+y
+
+newlist=reduce(sum,list1)
+print(newlist)
