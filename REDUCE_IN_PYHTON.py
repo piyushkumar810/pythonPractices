10d4ccbbf56aaa1bc0551a2851cbdef6efb271f1
piyushkumar810 <140537940+piyushkumar810@users.noreply.github.com>
2024-08-26 07:51:28 +0530

    reduce by using lambda function

--- REDUCE_IN_PYHTON.py
+++ REDUCE_IN_PYHTON.py
@@ -5,8 +5,12 @@
 from functools import reduce
 list1=[4,6,8,3,5,8,3]
  
-def sum(x,y):
-    return x+y
+# def sum(x,y):
+#     return x+y
 
-newlist=reduce(sum,list1)
+# newlist=reduce(sum,list1)
+# print(newlist)
+
+# ------------or by lambda function
+newlist=reduce(lambda x,y: x+y, list1)
 print(newlist)
