a703d4624c39bea41b50065ef754b11002c03ac3
piyushkumar810 <140537940+piyushkumar810@users.noreply.github.com>
2024-08-27 07:59:05 +0530

    updated

---
+++ Projects/Snale_Water_Gun_GAME.py
@@ -0,0 +1,6 @@
+# ------------------------------------------snake water gun game
+# 1. snake, water and gun game is a variation of children's game like "rock, paper and scissore" where player
+#    use their hand gestures to represent a stone, paper or scessire, in this game the gun beats the skane
+#   ,the water beats the gun,  and the snake beats the water.
+
+# 2.  write a python program to create a snake water and gun game using proper function.
--- REDUCE_IN_PYHTON.py
+++ REDUCE_IN_PYHTON.py
@@ -5,12 +5,12 @@
 from functools import reduce
 list1=[4,6,8,3,5,8,3]
  
-# def sum(x,y):
-#     return x+y
+def sum(x,y):
+    return x+y
 
-# newlist=reduce(sum,list1)
-# print(newlist)
+newlist=reduce(sum,list1)
+print(newlist)
 
 # ------------or by lambda function
-newlist=reduce(lambda x,y: x+y, list1)
-print(newlist)
+newlist1=reduce(lambda x1,y1: x1+y1, list1)
+print(newlist1)
