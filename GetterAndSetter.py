cfdf899ada9fc85b7b2a6bef4246f431b85bb63c
piyushkumar810 <140537940+piyushkumar810@users.noreply.github.com>
2024-09-16 22:01:11 +0530

    changed value using setter

--- GetterAndSetter.py
+++ GetterAndSetter.py
@@ -11,3 +11,19 @@ a=myclass(10)
 print(a.value)
 
 # setter --> it is important to note that getter do not take any parameter and we cannot set value through getter for that we need setter which can be added by  decorater method with @property_name.setter
+
+class Myclass:
+    def __init__(self,values):
+        self._values=values
+    
+    @property
+    def values(self):
+        return self._values
+    
+    @values.setter
+    def values(self, new_values):
+        self._values=new_values
+
+obj=Myclass(10)
+obj.values=20
+print(obj.values)
