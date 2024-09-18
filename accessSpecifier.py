fe78a45f5e8288bd257fabbb539c01dfcf098fbf
piyushkumar810 <140537940+piyushkumar810@users.noreply.github.com>
2024-09-18 22:59:42 +0530

    access specifier in python

---
+++ accessSpecifier.py
@@ -0,0 +1,9 @@
+#---------------------------------- Access specifier in python
+
+# ------------public access specifier
+class employees:
+    def __init__(self):
+        self.name="piyush"
+
+a=employees()
+print(a.name)
