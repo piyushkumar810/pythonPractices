ccaee4d394145fe448f6d1aa7db23149ed87ae20
piyushkumar810 <140537940+piyushkumar810@users.noreply.github.com>
2024-10-16 07:59:11 +0530

    updated lms

--- Projects/libraryManaagementSystem.py
+++ Projects/libraryManaagementSystem.py
@@ -1,4 +1,8 @@
 # --------------------------------------library management system------------------------------
+# write a library class with no of books and books as two instance variable. write a program to create a library
+# from the library class and show how you can print all the books add a boook and get the number of books 
+# using different method. show that your program does not presist the book after the program is stopped.
+
 
 class library:
     def __init__(self):
