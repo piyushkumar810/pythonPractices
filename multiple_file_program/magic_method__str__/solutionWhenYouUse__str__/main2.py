652de1d7ef6de7e9fe7d11bc5f9447d3b8353b06
piyushkumar810 <140537940+piyushkumar810@users.noreply.github.com>
2024-10-18 15:52:14 +0530

    updated

--- multiple_file_program/magic_method__str__/solutionWhenYouUse__str__/main2.py
+++ multiple_file_program/magic_method__str__/solutionWhenYouUse__str__/main2.py
@@ -1,3 +1,8 @@
 from emp2 import employee
 e=employee("piyush")
 print(e)
+
+# -----------what does __str__() and __repr__() method do
+# the str and repr method are both used to convert object to string representation, but the main difference 
+# between repr and str is if python did'nt find __str__() method and if __repr__() is present then pyhton 
+# will fallback to __repr__()
