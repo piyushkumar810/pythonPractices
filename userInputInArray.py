74f87910c7c1adbc81f2b288d3a60cdb72369b58
piyushkumar810 <140537940+piyushkumar810@users.noreply.github.com>
2024-10-21 08:05:06 +0530

    searching element of an array

---
+++ userInputInArray.py
@@ -0,0 +1,25 @@
+# --------------------- User input in an array
+
+from array import *
+
+arr=array('I', [])
+
+n=int(input("enter the length of array ="))
+
+for i in range(n):
+    x=int(input("enter the value ="))
+    arr.append(x)
+    i=i+i
+
+print(arr)
+
+# searching the value given by the user
+user=int(input("enter the value you want to search ="))
+
+k=0
+
+for e in arr:
+    if(e==user):
+        print(f'your desiered value {user} is at {k}th index')
+        break
+    k=k+1
