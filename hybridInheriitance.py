f120f8f46752d85a447464b230e893aada6f80ce
piyushkumar810 <140537940+piyushkumar810@users.noreply.github.com>
2024-10-24 15:53:34 +0530

    hybrid inheritance

---
+++ hybridInheriitance.py
@@ -0,0 +1,19 @@
+# -------------------------hybrid inheritance in python
+
+class A:
+    def work(self):
+        print('i can  work')
+
+class B(A):
+    def sleep(self):
+        print('i can sleep whole day')
+
+class C:
+    def play(self):
+        print("my fevorite game is cricket")
+
+class D(B,C):
+    pass
+
+obj_D=D()
+obj_D.work()
