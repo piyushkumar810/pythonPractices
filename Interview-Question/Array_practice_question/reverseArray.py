d11749b96566f9c7cadd883324692e07f286fea6
piyushkumar810 <140537940+piyushkumar810@users.noreply.github.com>
2024-10-25 06:34:02 +0530

    reverse of an array

---
+++ Interview-Question/Array_practice_question/reverseArray.py
@@ -0,0 +1,12 @@
+# ----------------------------reverse an array
+
+from array import *
+
+arr=array('I', [2,5,7,6,9])
+
+a=len(arr)
+for i in range (a//2):
+    tem=arr[i]
+    arr[i]=arr[a-i-1]
+    arr[a-i-1]=tem
+print(arr)
