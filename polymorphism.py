41b81f2907f01f62a0dd784062f96e7cf0f8e58c
piyushkumar810 <140537940+piyushkumar810@users.noreply.github.com>
2024-10-28 20:30:01 +0530

    polymorphysm in pyhton

---
+++ polymorphism.py
@@ -0,0 +1,30 @@
+# ----------------------polymorphism in python
+
+# polymorphism means that different object can be treated as if they are of same type, allowing them
+# to respond to the same method or function in their own way
+
+
+# eg
+
+print(1+2)
+print("1" + "2")
+# see here, you know in pyhton everything is object in this example bith place i am using + operator
+# but woorking of both line is different
+print("\n")
+
+# eg2
+list1=[4,"piyush",4.5, True]
+print(list1)
+print(type(list1))
+print("\n")
+
+dict1={
+    1:"piyush",
+    2:"kumar",
+    3:"raj",
+    4:"rohit"
+}
+print(dict1)
+print(type(dict1))
+print("\n")
+
