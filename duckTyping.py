993f81c595b224441202bb26450a9936a65701bd
piyushkumar810 <140537940+piyushkumar810@users.noreply.github.com>
2024-10-28 22:19:13 +0530

    polymorphism

---
+++ duckTyping.py
@@ -0,0 +1,19 @@
+# -----------------------polymorphism can be implemented by four ways
+
+# 1st --> duck typing
+# 2nd --> method overloading
+# 3rd --> operator overloading
+# 4th --> method overriding
+
+# 1st ------------------ duck typing
+
+# in python duck typing feature used because it allowed the data type to be dynamic
+class duck:
+    def quack(self):
+        return "quake !"
+    
+class cat:
+    def quack(self):
+        return "meow !"
+    
+    def make_it_quack()
