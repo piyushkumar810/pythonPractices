94b20b867fd98baf6892e4a8d353b543409d761d
piyushkumar810 <140537940+piyushkumar810@users.noreply.github.com>
2024-11-06 17:18:48 +0530

    finding kth element from sorted array

--- Interview-Question/Array_practice_question/find_Kth_MaxAndMin.py
+++ Interview-Question/Array_practice_question/find_Kth_MaxAndMin.py
@@ -1,1 +1,21 @@
 # ---------------------------find kth max and min element in array
+
+from array import *
+
+class solution:
+    def kthSmallest(self, arr, k, l):
+         newarr=sorted(arr)
+         print(newarr)
+
+         for i in range(l-1):
+              if(i==(k-1)):
+                   print(newarr[i])
+
+sol=solution()
+arr=array('I', [7,3,4,6,25,20])
+l=len(arr)
+# print(l)
+
+k=int(input("enter the value and the value should be less than the size of the array : "))
+
+sol.kthSmallest(arr,k,l)
