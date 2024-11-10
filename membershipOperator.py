9704fe7ea0418fb73bbec3bfe044203b9b2487af
piyushkumar810 <140537940+piyushkumar810@users.noreply.github.com>
2024-11-10 18:27:05 +0530

    membership operator

---
+++ membershipOperator.py
@@ -0,0 +1,13 @@
+# in & not in --> are called membership operator 
+# it is used to check members of (char, sub_string, value , variable) is present in that sequence or not
+
+str="piyush kumar"
+
+print('p' in str)
+print('z' in str)
+print('p' not in str)
+
+print("\n")
+l=[4,6,83, "piyush", 45.5]
+print("piyush" in l)
+print("p" in l)
