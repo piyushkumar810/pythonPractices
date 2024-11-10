25c653bba3e8ed533f31259b17983df42cae4781
piyushkumar810 <140537940+piyushkumar810@users.noreply.github.com>
2024-11-10 18:06:08 +0530

    args and kwargs

---
+++ args_And_kwargs.py
@@ -0,0 +1,27 @@
+# -------------------------------   *args and **kwargs in python
+
+# --------- *args
+# it allow you to pass variable number  of non keyword argument to a function
+#  it takes all the argument in tuple form(tuple is immutable) 
+# eg
+def my_fun(*args):
+    c=0
+    for i in args:
+        c+=i
+    print(f"sum is {c}")
+
+
+my_fun(1,2)
+my_fun(1,5,78,4)
+my_fun(4,6,8,3,56,7,4,4)
+print("\n")
+
+# ----------- **kwargs
+#  it allow you to pass variable number  of keyword argument to a function
+# it stores all the argument in the form of dictionary
+# eg
+def my_fun1(**kwargs):
+    for key,value in kwargs.items():
+        print(f" {key } = { value}")
+
+my_fun1(name="piyush" , age=22, state="jharkhand")
