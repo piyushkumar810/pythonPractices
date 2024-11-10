63429822d3f4ff1e49431f857d0dc5fc7bf34e80
piyushkumar810 <140537940+piyushkumar810@users.noreply.github.com>
2024-11-10 17:48:57 +0530

    round function

---
+++ roundFunction.py
@@ -0,0 +1,34 @@
+# it specifies no of decimal place or to nearest integer
+
+print(round(7))
+print(type((round(7))))
+
+print(round(7,2))
+print(type((round(7,2))))
+
+print(round(7.61))
+print(type((round(7.61))))
+
+print(round(7.66666,2))
+print(type((round(7.66666,2))))
+print("\n")
+
+print(round(7.5))
+# in python it will return nearest even integer
+print(round(6.5))
+print("\n")
+
+print(round(674))
+print(round(674,0))
+print(round(674,-1))
+# this -1 will affect 
+print(round(674,-2))
+print(round(674,-3))
+print(round(674,-4))
+print("\n")
+
+print(round(-8/3))
+print(type((round(-8/3))))
+
+print(round(-8/3,2))
+print(type((round(-8/3,2))))
