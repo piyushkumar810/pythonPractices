459a723a4851a0098ecf9f72c670fd56bdb43346
piyushkumar810 <140537940+piyushkumar810@users.noreply.github.com>
2024-11-17 17:40:14 +0530

    not solved

---
+++ Projects/combining_multiple_pdf_into_single/main.py
@@ -0,0 +1,21 @@
+# Q) write a program to manipulate pdf using pyPDF. your 
+#       program should be able to merge multiple files into a single 
+#       pdf. you are welcome to add more functionality
+
+
+# what is pyPDF?
+'''pyPDF is a free and open source pure-python PDF capable of 
+   splitting, merging, cropping, and transforming the pages of pdf
+   file. it can add custome data , viewing option, and password to pdf files.
+   pyPDF can retrieve text and metaData from PDFs as well'''
+
+
+# from pyPDF2 import PdfWriter
+
+# merger = PdfWriter()
+
+# for pdf in ["file1.pdf", "file2.pdf", "file3.pdf"]:
+#     merger.append(pdf)
+
+# merger.write("merged-pdf.pdf")
+# merger.close()
