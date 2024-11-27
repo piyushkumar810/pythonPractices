9b826592b0c469c81ab37f9ffd7e410e48c8c9b2
piyushkumar810 <140537940+piyushkumar810@users.noreply.github.com>
2024-11-27 17:23:41 +0530

    hexagon in pyhton graphics

---
+++ turtal_graphics/hexagon.py
@@ -0,0 +1,26 @@
+import turtle
+turtle.getscreen()
+t=turtle.Turtle()
+
+# making hexagon
+# Pentagon has 6 sides; each exterior angle is 60° (360° ÷ 6)
+t.forward(100)
+t.left(60)
+
+t.forward(100)
+t.left(60)
+
+t.forward(100)
+t.left(60)
+
+t.forward(100)
+t.left(60)
+
+t.forward(100)
+t.left(60)
+
+t.forward(100)
+t.left(60)
+
+
+turtle.done()
