7d9ce8a181e251dd0eb6c88db45b3da572921cb0
piyushkumar810 <140537940+piyushkumar810@users.noreply.github.com>
2024-11-27 17:25:42 +0530

    heptagon in python graphics

---
+++ turtal_graphics/heptagon.py
@@ -0,0 +1,29 @@
+import turtle
+turtle.getscreen()
+t=turtle.Turtle()
+
+# making heptagon
+# Pentagon has 7 sides; each exterior angle is 51° (360° ÷ 7)
+t.forward(100)
+t.left(51)
+
+t.forward(100)
+t.left(51)
+
+t.forward(100)
+t.left(51)
+
+t.forward(100)
+t.left(51)
+
+t.forward(100)
+t.left(51)
+
+t.forward(100)
+t.left(51)
+
+t.forward(100)
+t.left(51)
+
+
+turtle.done()
