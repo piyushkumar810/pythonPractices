ba80631223d86874a36bedd28db6db53204c55e9
piyushkumar810 <140537940+piyushkumar810@users.noreply.github.com>
2024-11-27 17:03:41 +0530

    making rectangle in turtle graphics

---
+++ turtal_graphics/rectangle.py
@@ -0,0 +1,12 @@
+import turtle
+turtle.getscreen()
+
+turtle.forward(150)
+turtle.left(90)
+turtle.forward(70)
+turtle.left(90)
+turtle.forward(150)
+turtle.left(90)
+turtle.forward(72)
+
+turtle.done()
