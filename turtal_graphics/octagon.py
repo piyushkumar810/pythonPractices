845eb09905449afa016822784d0732efb3eabd58
piyushkumar810 <140537940+piyushkumar810@users.noreply.github.com>
2024-11-27 17:28:55 +0530

    octagon in python graphics

---
+++ turtal_graphics/octagon.py
@@ -0,0 +1,32 @@
+import turtle
+turtle.getscreen()
+t=turtle.Turtle()
+
+# making octagon
+# Pentagon has 8 sides; each exterior angle is 45° (360° ÷ 8)
+t.forward(100)
+t.left(45)
+
+t.forward(100)
+t.left(45)
+
+t.forward(100)
+t.left(45)
+
+t.forward(100)
+t.left(45)
+
+t.forward(100)
+t.left(45)
+
+t.forward(100)
+t.left(45)
+
+t.forward(100)
+t.left(45)
+
+t.forward(100)
+t.left(45)
+
+
+turtle.done()
