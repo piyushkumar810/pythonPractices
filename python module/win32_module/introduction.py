cef239a12a755e51443c8f36e9006f3a69468a2f
piyushkumar810 <140537940+piyushkumar810@users.noreply.github.com>
2024-12-16 16:40:02 +0530

    about win32 module

---
+++ python module/win32_module/introduction.py
@@ -0,0 +1,17 @@
+#-------------------------- introduction of win32 module ?
+# The Windows API provides a vast set of functionalities for interacting with the Windows operating 
+# system, including file handling, window management, and more.
+
+# pywin32 includes several modules such as :-
+#             1. win32api
+#             2. win32gui
+#             3. win32con, and 
+#             4. win32com. You can import and use them based on your requirements.
+
+
+# --------------------------------------- When to Use Each Module
+#                 Module	Primary Use
+#               win32api	System-level tasks (e.g., file handling, system info, basic message boxes).
+#               win32gui	GUI operations, such as managing windows, dialogs, and handling events.
+#               win32con	Provides constants needed for win32api and win32gui functions.
+#               win32com	Automating and interacting with Windows applications and other COM objects.
