1798048d84a5fbba62ed859ec4ccd80798a2a0b4
piyushkumar810 <140537940+piyushkumar810@users.noreply.github.com>
2025-01-03 18:34:00 +0530

    practice

---
+++ Interview-Question/2nd_interview.py
@@ -0,0 +1,34 @@
+# Q1) what is pyhton?
+'''ans -> python is a versitile and high level programmig language which is known for its simplicity and 
+         redability. it is widely used in various domain llike web- development , app devlopment
+         data analsis, artifical intiligence and so on: 
+          it is easy to use.
+          interpreted language.
+          dynamic typed - means we don't need to declear variable explicitly.
+          oop feature.
+          extensive libraries.'''
+
+# Q2) difference between map and filter?
+''' ans -> map -> the map() function is used to apply a specific function to every item in an iterable 
+                  (like a list, tuple, or set) and return a map object (an iterator).
+                  
+                  the function applied to all elements must return a value.
+                  
+                  here output size is same as the input size.
+                  
+        filter -> The filter() function in Python is used to filter elements from an iterable based on 
+                  a condition specified by a function.
+                  
+                  the function must return a boolean (True or False).
+                  
+                  here output size may decreases because of filitration.'''
+
+# Q3) what are loop control statement in python?
+''' ans -> the loop control statement in python alloow you to alter the normal flow of a loop.
+           there are 3 types of loop control statement in pyhton.
+           
+           1st -> break statement (exit form the loop permenently if condition met.)
+           2nd -> continue statement(skips the current intration and goes to the next iteration.)
+           3rd -> pass statement (it does nothing and allows the code to continue running)'''
+
+# Q4) what is pip and pep?
