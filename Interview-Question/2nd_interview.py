d3797a9e9e62eb90c54f529c642dd2744f879d3f
piyushkumar810 <140537940+piyushkumar810@users.noreply.github.com>
2025-01-04 09:16:46 +0530

    practice

--- Interview-Question/2nd_interview.py
+++ Interview-Question/2nd_interview.py
@@ -31,4 +31,48 @@
            2nd -> continue statement(skips the current intration and goes to the next iteration.)
            3rd -> pass statement (it does nothing and allows the code to continue running)'''
 
-# Q4) what is pip and pep?
+# Q4) what are keywprds in pyhton?
+''' ans -> the keyword are the reversed word which are used as identifiers, function name and more.
+
+           they help define the structure and syntax of the language.
+
+           eg:- False, True, and, if, elif, def, break, continue, return, pass, for etc...'''
+
+# Q5) what are literals in python?
+''' ans -> literals in python refers to the data that is given in a vriable or constant.
+
+           there are four types of literals in python: 
+           
+           1st -> string literal:  sequence of character enclosed within quotes.
+           2nd -> numeric literal: integer, float and complex number.
+           3rd -> boolean literal: represent True or False.
+           4th -> special literal: "None" is a good example.'''
+
+# Q6) how can you concatenate two tuples?
+'''sol -> simply we use + (addition symbol)
+           eg:-
+               tuple1=(1,"piyush", 32.5)
+               tuple2=(True, 2, None)
+
+               print(tuple1 + tuple2)  // (1, "piyush", 32.5, True, 2, None)
+               print(tuple2 + tuple1)  // (True, 2, None, 1, "piyush", 32.5)''' 
+
+# Q7) how can you initialize a 5*5 NumPy array with only zeros?
+''' sol -> NumPy has a brilliant function - zeros() that do this:-
+
+           import numpy as np
+           n1= np.zeros((5,5))
+           print(n1)                    //  [[0,0,0,0,0]
+                                             [0,0,0,0,0]
+                                             [0,0,0,0,0]
+                                             [0,0,0,0,0]
+                                             [0,0,0,0,0]]'''
+        
+# Q8) what is pandas?
+''' ans-> pandas is an open-source library that has a very rich set of data structure for data based
+          operation.
+          
+          pandas can deals with a large varities of files and is one of the most important tools
+          to have a grip on.
+          
+          it provides two fantastic data structure - Series and DataFrames.'''
