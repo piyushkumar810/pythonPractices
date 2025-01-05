20308e0bbaea22386990247ffe8babc911661e3f
piyushkumar810 <140537940+piyushkumar810@users.noreply.github.com>
2025-01-05 08:11:07 +0530

    intro about pandas

---
+++ python_libraries/PANDAS/intro.py
@@ -0,0 +1,24 @@
+# 	What is pandas?
+'''
+•	It is one of the most important python library that is used for data analysis.
+•	It was created by wes mckinney in 2008.
+•	It has functions for analyzing, cleaning, exploring and manipulating data.
+•	It can read and data structure of different formats like:- csv, Json, zip, text, xml etc.(matlab Kisi v file formate k saath kam ker Sakata hai).
+'''
+
+# Pandas data structure
+'''
+     There are three data structure through which we can analysis data  in pandas:
+1.	Series             - One-Dimensional labeled array (like data)
+2.	Data Frames        - Two-Dimensional data structure with columns, much likely a tables.
+3.	Panel              - a panel is a 3-D container of data.
+'''
+
+# Installation and import pandas
+'''                         
+            INSTALLATION
+         pip install pandas
+
+                 IMPORT
+         Import pandas as pd
+'''
