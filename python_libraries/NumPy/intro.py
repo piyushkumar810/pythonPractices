7473fb605277bf7650165d94f7e1f94d65a7dae1
piyushkumar810 <140537940+piyushkumar810@users.noreply.github.com>
2025-01-13 13:54:54 +0530

    NumPy library in python

---
+++ python_libraries/NumPy/intro.py
@@ -0,0 +1,21 @@
+# ----------------------------------- introduction (lec-01)
+
+'''
+	NumPy is the fundamental package for scientific computing in python.
+	NumPy is a python library that provides a multidimensional array object, various derived object.
+	An array is a grid of values and it contains information about the raw data, how to locate an element,  and how to interpret an element.
+'''
+
+import numpy as np
+
+arr=np.array([2,4,6,8,4])
+print(arr)
+print(type(arr))
+
+'''
+Installation of NumPy:
+	Pip install numpy
+
+ Import NumPy:
+	Import numpy as np
+'''
