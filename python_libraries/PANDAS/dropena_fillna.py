e2dc5905057d0af47320daa097b54bf65d69a15d
piyushkumar810 <140537940+piyushkumar810@users.noreply.github.com>
2025-01-13 12:12:07 +0530

    updated

--- python_libraries/PANDAS/dropena_fillna.py
+++ python_libraries/PANDAS/dropena_fillna.py
@@ -7,6 +7,8 @@ import pandas as pd
 csv_1 = pd.read_csv("C:\\Users\\piyush kumar\\Downloads\\Employee_Records.csv")
 # print(csv_1)
 
+# ----------------------------------------------------- dropena
+
 # i want that the missing data should be dropped(means wherever "NaN" is written that field should be dropped)
 # ye along row data ko drop kiya
 # print(csv_1.dropna())
@@ -28,5 +30,23 @@ print("\n")
 
 # print(csv_1.dropna(inplace=True))
 
-# if you want to delete only that row or column which has only one null value (that means the row which consist of multiple null value will not be deleted)
-print(csv_1.dropna(thresh=2))
+# if you want to delete only that row or column which jitna ki thresh diya hua hai value (that means the row which consist of multiple null value will not be deleted)
+# print(csv_1.dropna(thresh=2))
+
+# --------------------------------------- fillena
+
+# if you want to fill all null value with a particular value
+# print(csv_1.fillna("python"))
+
+# if you want that paticular column to be filled with specific value
+# print(csv_1.fillna({"Employee Name": "pyhton", "Employee Salary": "c " , "Department":"java"}))
+
+# if you want that the blank data should be filled with forward or backward data
+# print(csv_1.fillna(method="ffill"))
+# print(csv_1.fillna(method="bfill"))
+# print(csv_1.fillna(method="ffill", axis=1))
+
+# if you want to fill some value in place of all NaN value
+# print(csv_1.fillna(12, inplace=True))
+# print(csv_1)
+
