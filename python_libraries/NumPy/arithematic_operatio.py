1d77f9fd79f0fc4e8d9f0e7ca67f8f8815a262a1
piyushkumar810 <140537940+piyushkumar810@users.noreply.github.com>
2025-01-18 12:06:22 +0530

    arithematic

---
+++ python_libraries/NumPy/arithematic_operatio.py
@@ -0,0 +1,56 @@
+# -------------------------- arithematic operatin in numpy arrray(lec-07)
+
+# a+b      -->  np.add(a,b)
+# a-b      -->  np.subtract(a,b)
+# a*b      -->  np.multiply(a,b)
+# a/b      -->  np.divide(a,b)
+# a%b      -->  np.mod(a,b)
+# a**b     -->  np.power(a,b)
+# 1/a      -->  np.reciprocal(a,b)
+
+
+import numpy as np
+# ---------------------------addition
+var1=np.array([1,2,3,4,5])
+var2=np.array([6,7,8,9,5])
+sum=np.add(var1,var2)
+# sum=var1+var2
+print(sum)
+print("\n")
+
+
+# ------------------------- substract
+varS1=np.array([1,2,3,4,5])
+varS2=np.array([6,7,8,9,5])
+sub=np.subtract(varS1,varS2)
+# sub=varS2-varS1
+print(sub)
+print("\n")
+
+# ------------------------- multiply
+varM1=np.array([1,2,3,4,5])
+varM2=np.array([6,7,8,9,5])
+mul=np.multiply(varM1,varM2)
+print(mul)
+print("\n")
+
+# -------------------------- divide
+varD1=np.array([1,2,3,4,5])
+varD2=np.array([6,7,8,9,5])
+div=np.divide(varD1,varD2)
+print(div)
+print("\n")
+
+# -------------------------- modulo division
+varMD1=np.array([1,2,3,4,5])
+varMD2=np.array([6,7,8,9,5])
+Mdiv=np.mod(varMD1,varMD2)
+print(Mdiv)
+print("\n")
+
+# -------------------------- power
+varP1=np.array([1,2,3,4,5])
+varP2=np.array([6,7,8,9,5])
+pow=np.pow(varP1,varP2)
+print(pow)
+print("\n")
