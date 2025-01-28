4b666c475c4cdc39b450c6ae1eccf12d15587c26
piyushkumar810 <140537940+piyushkumar810@users.noreply.github.com>
2025-01-28 22:02:11 +0530

    practice question

---
+++ Interview-Question/predict_the_output1.py
@@ -0,0 +1,16 @@
+l1=[20,30,40]
+l2=5
+print(l1*l2)
+
+# explanation
+'''
+In Python, multiplying a list by an integer (like l1 * l2) creates a new list where the original list 
+(l1) is repeated l2 times.
+
+Python’s * operator for lists is overloaded to perform repetition.
+l1 * l2 means "repeat the list l1 exactly l2 times."
+
+-----------output
+[20, 30, 40, 20, 30, 40, 20, 30, 40, 20, 30, 40, 20, 30, 40]
+
+'''
