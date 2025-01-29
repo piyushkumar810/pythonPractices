1572f7ee470fbc3281e0be7d094092c20181a28c
piyushkumar810 <140537940+piyushkumar810@users.noreply.github.com>
2025-01-29 21:54:35 +0530

    practice question

---
+++ python_libraries/NumPy/interview_question/q1.py
@@ -0,0 +1,10 @@
+# predict the output
+
+import numpy as np
+a=np.array([10,20,"30"])
+
+print(a)
+print(type(a))
+# what is the type of a
+# ans--> type of a is array only but the value inside that variable is in string now
+# output --> ['10' '20' '30']
