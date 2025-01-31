220e35919d0f1fd7bec6302c095c30620697ad36
piyushkumar810 <140537940+piyushkumar810@users.noreply.github.com>
2025-01-31 07:30:54 +0530

    introduction of matplotlib

---
+++ python_libraries/matplotlib/intro.py
@@ -0,0 +1,16 @@
+# ---------------------------------- Matplotlib python libraries
+''' 
+--> with previous liberaries we learned that how data is analyzed, cleaned, exploring and manipulating data.,
+            "but here with the help of matplotlib we will visualize the data(data visualization--> it is the graphical 
+            representation of information and data. in the form of graphs like :- piechart, barchart, stackplot, histogram etc..)"
+
+--> matplotlib is a low-level graph plotting library in python that server as a visualization utility.
+
+-->matplotlib was created by jhon D Hunter.
+
+-->matplotlib is a open-source and we can use it freely.
+
+-->to use this you need to download -------------------------  pip install matplotlib
+
+-->after downloading you need to import it to use in program - import matplotlib as mt 
+'''
