5de0527f58debeaad21f0e08741394fcd0b03229
piyushkumar810 <140537940+piyushkumar810@users.noreply.github.com>
2025-02-02 10:56:49 +0530

    saving these charts in our desktop

---
+++ python_libraries/matplotlib/last_how _to_save_these_charts_in_desktop.py
@@ -0,0 +1,18 @@
+# ------------------- How to save these charts in your desktop using matplotlib
+
+import matplotlib.pyplot as plt
+
+brands = ["OnePlus", "Apple", "Vivo", "Redmi", "Samsung"]
+users = [13, 25, 22, 18, 22]
+
+ex=[0,0.1,0,0,0]
+
+plt.figure(figsize=(7, 7))  # Set figure size
+plt.pie(users, labels=brands, autopct="%1.1f%%", startangle=140, colors=["red", "blue", "green", "orange", "purple"], explode=ex, shadow=True)
+plt.title("Market Share of Smartphone Brands")
+
+# plt.savefig -> this is used to save the figure, 
+                 # this contains lots of attribute like : pad_inches= 0.7, bbox_inches="tight" and more ...
+plt.savefig("pie.png")
+
+plt.show()
