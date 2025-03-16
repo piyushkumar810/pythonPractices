aa48d08eacc6f47ac6a263ff915eb85e416ab8a3
piyushkumar810 <140537940+piyushkumar810@users.noreply.github.com>
2025-03-16 21:19:32 +0530

    abstraction

---
+++ practice23.py
@@ -0,0 +1,7 @@
+# abstraction in python
+# hinding  complex implementation and only providing essential details.
+# python by default does not suppport abstraction you neeed to import it "import abc class"
+
+# to implement abstract class in python you need to create atleast one abstract method 
+# remember abstract method have only declearation no defination
+# abstraction method is creaated using decorator
