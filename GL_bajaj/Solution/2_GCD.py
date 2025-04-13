45909ae89ff11fc8b95a3983a272b0d43d620026
piyushkumar810 <140537940+piyushkumar810@users.noreply.github.com>
2025-04-13 06:21:41 +0530

    gcd

--- GL_bajaj/Solution/2_GCD.py
+++ GL_bajaj/Solution/2_GCD.py
@@ -1,5 +1,43 @@
 
 # -------------------if you enter two value
+# def gcd(a, b):
+#     while b != 0:
+#         temp = b
+#         remainder = a % b
+#         a = temp
+#         b = remainder
+#     return a
+
+# # Taking input
+# a = int(input("Enter first value: "))
+# b = int(input("Enter second value: "))
+
+# # Calling function and printing result
+# result = gcd(a, b)
+# print("GCD is:", result)
+
+
+# # ----------------- if you have to find three number GCD
+# def gcd(a, b):
+#     while b != 0:
+#         temp = b
+#         remainder = a % b
+#         a = temp
+#         b = remainder
+#     return a
+
+# # Taking input
+# a = int(input("Enter first value: "))
+# b = int(input("Enter second value: "))
+# c = int(input("Enter third value: "))
+
+# # Calling function and printing result
+# FirstRes = gcd(a, b)
+# result = gcd(FirstRes,c)
+# print("GCD is:", result)
+
+
+# ------------list of numbers
 def gcd(a, b):
     while b != 0:
         temp = b
@@ -8,10 +46,12 @@ def gcd(a, b):
         b = remainder
     return a
 
-# Taking input
-a = int(input("Enter first value: "))
-b = int(input("Enter second value: "))
+# Input list of numbers
+nums = list(map(int, input("Enter numbers separated by space: ").split()))
 
-# Calling function and printing result
-result = gcd(a, b)
-print("GCD is:", result)
+# Calculate GCD for entire list
+current_gcd = nums[0]
+for num in nums[1:]:
+    current_gcd = gcd(current_gcd, num)
+
+print("GCD of all numbers is:", current_gcd)
