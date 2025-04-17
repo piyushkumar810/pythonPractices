1fcf217830b74c7abe29708df294a44816927b97
piyushkumar810 <140537940+piyushkumar810@users.noreply.github.com>
2025-04-17 22:42:33 +0530

    string

---
+++ GL_bajaj/Solution/7_permutationOfString.py
@@ -0,0 +1,27 @@
+# What is a permutation of a string?
+'''
+   A permutation is a rearrangement of the characters in the string.
+   For example, for the string "abc", all possible permutations are:
+
+   abc
+   acb
+   bac
+   bca
+   cab
+   cba
+
+'''
+
+def permute(s, answer=""):
+    if len(s) == 0:
+        print(answer)
+        return
+
+    for i in range(len(s)):
+        ch = s[i]
+        left = s[:i]
+        right = s[i+1:]
+        rest = left + right
+        permute(rest, answer + ch)
+        
+permute("abc")
