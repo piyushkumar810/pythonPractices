2e3f45c1fb4d58490f92f177441f33630c996862
piyushkumar810 <140537940+piyushkumar810@users.noreply.github.com>
2025-04-20 13:44:38 +0530

    explanation

--- GL_bajaj/Solution/7_permutationOfString.py
+++ GL_bajaj/Solution/7_permutationOfString.py
@@ -1,4 +1,5 @@
 # What is a permutation of a string?
+
 '''
    A permutation is a rearrangement of the characters in the string.
    For example, for the string "abc", all possible permutations are:
@@ -26,3 +27,14 @@ def permute(s, answer=""):
 
 str=input("enter the string you want : ")
 permute(str)
+
+
+# ---------for better understanding (slicing starts from index 0 and ends at index i-1.)
+
+
+# str1="length"
+# for i in range (len(str1)):
+#     print("this is 1st  ",str1[:i])
+#     print("this is 2nd  ",str1[i+1:])
+#     print("combined = ", str1[:i]+str1[i+1:])
+#     print()
