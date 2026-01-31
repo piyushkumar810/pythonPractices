ef676927fff3e5d37ca4f7dceefde63c3deed72d
piyushkumar810 <140537940+piyushkumar810@users.noreply.github.com>
2026-02-01 00:12:15 +0530

    questions_related to handling risky code

--- Best_senario_based_question_all_units/unit_3_exception_handling/question_set.py
+++ Best_senario_based_question_all_units/unit_3_exception_handling/question_set.py
@@ -45,3 +45,30 @@ print(sys.argv)
 
 (Assume arguments passed)
 '''
+
+# ---------------------------- (pyq questions)
+'''
+Q3 (a)
+Import specific system modules to:
+Get the current working directory
+List all files and directories in the current directory
+Delete a file
+Add days to the current date
+'''
+
+'''
+Q3 (b)
+Handle the following exceptions:
+Attempt to open a non-existing file
+Create a custom exception InvalidAgeError raised if age < 18
+Zero division error
+'''
+
+'''
+Q3 (c)
+A student.csv file is created with fields:
+SRN, Name, M1, M2, M3
+Write a Python program to:
+Read the file
+Display total and average marks of each student
+'''
