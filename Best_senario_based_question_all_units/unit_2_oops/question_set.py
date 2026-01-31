90eeea16c4518e8f5d3a242486de533575a6cb46
piyushkumar810 <140537940+piyushkumar810@users.noreply.github.com>
2026-02-01 00:07:22 +0530

    questions

--- Best_senario_based_question_all_units/unit_2_oops/question_set.py
+++ Best_senario_based_question_all_units/unit_2_oops/question_set.py
@@ -64,3 +64,28 @@ class Test:
 
 t = Test()
 del t
+
+# ------------------------- question set(pyq)
+'''
+Q2 (a)
+Write an Object-Oriented Program to create a class Complex with img, real and use operator overloading to multiply two complex numbers.
+'''
+
+'''
+Q2 (b)
+
+Write an OOP program in Python to create a class to read and add two distances.
+Use two member functions get_distance and show_distance
+Read and display distance in kms and mts
+Use a class variable that counts the different values stored
+Use add_distance to add distances
+'''
+
+'''
+Q2 (c)
+Write an Object-Oriented program in Python to create a class called person with data members name and age.
+Create an inherited class employee with data member department.
+
+Check whether a given person is an employee or not
+
+Display the average age of employees in a particular department'''
