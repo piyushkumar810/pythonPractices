ca63ef75bf7cbeb89c92544ce1f179e38bdfde3f
piyushkumar810 <140537940+piyushkumar810@users.noreply.github.com>
2026-01-31 23:59:59 +0530

    selenium

---
+++ Best_senario_based_question_all_units/unit_4/question_set.py
@@ -0,0 +1,47 @@
+# 🔹 UNIT–4
+# Data Ingestion & Multiprocessing (🔥 Toughest)
+# Section A – Database & APIs
+'''
+Write a Python program to:
+
+Connect to PostgreSQL
+
+Fetch records where salary > average salary
+
+Write a program to:
+
+Fetch data from a REST API
+
+Parse JSON
+
+Store selected fields in CSV
+
+Write a program to:
+
+Read data from Google Sheets
+
+Update a specific cell dynamically
+
+Section B – Web Scraping
+
+Write a Python program using requests + BeautifulSoup to:
+
+Scrape all links from a webpage
+
+Store them in a text file
+
+Section C – Multithreading & Multiprocessing
+
+Write a program to:
+
+Create 3 threads
+
+Each thread computes factorial of a number
+
+Write a Python program using multiprocessing to:
+
+Find sum of elements of an array in parallel
+
+import threading
+print(threading.current_thread().name)
+'''
