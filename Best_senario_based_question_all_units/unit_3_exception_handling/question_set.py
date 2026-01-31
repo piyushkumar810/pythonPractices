061e80c9ed99c39f66b393152f7f7aedb746cf7b
piyushkumar810 <140537940+piyushkumar810@users.noreply.github.com>
2026-01-31 23:58:20 +0530

    exceptional handling

---
+++ Best_senario_based_question_all_units/unit_3_exception_handling/question_set.py
@@ -0,0 +1,47 @@
+# 🔹 UNIT–3
+# Exception Handling, File I/O, Modules
+# Section A – Exceptions
+'''
+Q1.Write a program to:
+Create custom exception NegativeBalanceError
+Raise it when withdrawal exceeds balance
+'''
+
+'''
+Q2.Write a Python program that:
+Handles ValueError, ZeroDivisionError
+Uses else and finally blocks
+'''
+
+
+# Section B – File Handling (High Probability)
+'''
+Q3.Write a Python program to:
+Read a CSV file containing ID, Name, Salary
+Display highest paid employee
+'''
+
+'''
+Q4.Write a program to:
+Read JSON file
+Convert it to CSV
+'''
+
+'''
+Q5.Write a Python program using pickle to:
+Serialize employee objects
+Deserialize and display them
+'''
+
+# Section C – Modules
+'''
+Write a program using os module to:
+Create a directory
+Rename a file
+Delete empty directories
+
+import sys
+print(sys.argv)
+
+(Assume arguments passed)
+'''
