44336de07b3bdfa3c5913b244cd8610f55ddd819
piyushkumar810 <140537940+piyushkumar810@users.noreply.github.com>
2026-02-01 00:04:56 +0530

    related to fetching api

--- Best_senario_based_question_all_units/unit_4/question_set.py
+++ Best_senario_based_question_all_units/unit_4/question_set.py
@@ -2,44 +2,40 @@
 # Data Ingestion & Multiprocessing (🔥 Toughest)
 # Section A – Database & APIs
 '''
-Write a Python program to:
-
+Q1.Write a Python program to:
 Connect to PostgreSQL
-
 Fetch records where salary > average salary
+'''
 
-Write a program to:
-
+'''
+Q2.Write a program to:
 Fetch data from a REST API
-
 Parse JSON
-
 Store selected fields in CSV
+'''
 
-Write a program to:
-
+'''
+Q3.Write a program to:
 Read data from Google Sheets
-
 Update a specific cell dynamically
+'''
 
-Section B – Web Scraping
-
-Write a Python program using requests + BeautifulSoup to:
-
+# Section B – Web Scraping
+'''
+Q4.Write a Python program using requests + BeautifulSoup to:
 Scrape all links from a webpage
-
 Store them in a text file
+'''
 
-Section C – Multithreading & Multiprocessing
-
-Write a program to:
-
+# Section C – Multithreading & Multiprocessing
+'''
+Q5.Write a program to:
 Create 3 threads
-
 Each thread computes factorial of a number
+'''
 
-Write a Python program using multiprocessing to:
-
+'''
+Q6.Write a Python program using multiprocessing to:
 Find sum of elements of an array in parallel
 
 import threading
