789f588fc1e6ec59d5a15df6868a9fdbd9eee4eb
piyushkumar810 <140537940+piyushkumar810@users.noreply.github.com>
2026-02-02 02:06:30 +0530

    right angled triangle pattern

---
+++ Best_senario_based_question_all_units/unit_1/pattern_practice/right_angled triangle_downward.py
@@ -0,0 +1,13 @@
+# Q1)
+'''
+*****
+****
+***
+**
+*
+'''
+
+n=int(input("enter the row "))
+for i in range(1,n+1):
+    star=(n-(i-1))
+    print("*"*star)
---
+++ Best_senario_based_question_all_units/unit_1/pattern_practice/tempCodeRunnerFile.py
@@ -0,0 +1,5 @@
+
+n=int(input("enter the row "))
+for i in range(1,n+1):
+    star=(n-(i-1))
+    print("*"*star)
